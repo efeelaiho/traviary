e197c7dae74299e22326a280e60b51f852530730
efeelaiho <efeelaiho15@utexas.edu>
2016-03-11 18:27:30 -0600

    fixed drawable names

--- app/src/main/java/cs371m/traviary/MainActivity.java
+++ app/src/main/java/cs371m/traviary/MainActivity.java
@@ -1,6 +1,7 @@
 package cs371m.traviary;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,10 +13,19 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+    private boolean log_off;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        log_off = true;
+        if (log_off) {
+            Intent login_intent = new Intent(this,LoginActivity.class);
+            startActivity(login_intent);
+        }
+
 
     }
 
