baeeb8d67551cc9dc32270a2cbd937eb9645b0ed
efeelaiho <efeelaiho15@utexas.edu>
2016-04-03 19:45:59 -0500

    added Buttons to Home.java

--- app/src/main/java/cs371m/traviary/Home.java
+++ app/src/main/java/cs371m/traviary/Home.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.widget.Button;
 
 
 /**
@@ -14,7 +15,8 @@ import android.support.v4.app.Fragment;
  */
 public class Home extends Fragment {
 
-
+    private Button mLocationButton;
+    private Button mAttractionButton;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +24,10 @@ public class Home extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v =inflater.inflate(R.layout.home,container,false);
+        View v = inflater.inflate(R.layout.home,container,false);
+        mLocationButton = (Button)v.findViewById(R.id.location_button);
+        mAttractionButton = (Button)v.findViewById(R.id.attraction_button);
+
         return v;
     }
 }
