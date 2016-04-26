e2c1711beb07e205072991c5a211beb6b55bda5e
Jong Hoon Lim <jhlim84@hotmail.com>
2016-04-26 10:22:08 -0500

    Correctly receiving user's latitude, longitude and overall location using FusedLocationAPI.

--- app/src/main/java/cs371m/traviary/MyLocationActivity.java
+++ app/src/main/java/cs371m/traviary/MyLocationActivity.java
@@ -16,19 +16,30 @@
 
 package cs371m.traviary;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.*;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * This demo shows how GMS Location can be used to check for changes to the users location.  The
@@ -40,7 +51,9 @@ public class MyLocationActivity extends AppCompatActivity
         implements
         OnMyLocationButtonClickListener,
         OnMapReadyCallback,
-        ActivityCompat.OnRequestPermissionsResultCallback {
+        ActivityCompat.OnRequestPermissionsResultCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
     /**
      * Request code for location permission request.
@@ -57,6 +70,10 @@ public class MyLocationActivity extends AppCompatActivity
 
     private GoogleMap mMap;
 
+    private GoogleApiClient mGoogleApiClient;
+
+    private android.location.Location mLastLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,6 +82,84 @@ public class MyLocationActivity extends AppCompatActivity
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        double latitude = mLastLocation.getLatitude();
+        double longitude = mLastLocation.getLongitude();
+        String currentCity = "";
+        String currentState = "";
+        String currentCountry = "";
+        System.out.println("LATITUDE: " + latitude); // ***** DEBUG *****
+        System.out.println("LONGITUDE: " + longitude); // ***** DEBUG *****
+
+        LatLng currentLocation = new LatLng(latitude, longitude);
+        if(mMap != null){
+            /* move the camera to current location */
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 6.0f));
+        }
+
+        Geocoder geoCoder = new Geocoder(getBaseContext(), Locale.getDefault());
+        try {
+            /* get the city, state (if applicable), country */
+            List<Address> addresses = geoCoder.getFromLocation(latitude, longitude, 1);
+            if (addresses.size() > 0) {
+                currentCity += addresses.get(0).getLocality(); // city
+                currentState += addresses.get(0).getAdminArea(); // state
+                currentCountry += addresses.get(0).getCountryName(); // country
+                System.out.println("CURRENT CITY: " + currentCity); // ***** DEBUG *****
+                System.out.println("CURRENT STATE: " + currentState); // ***** DEBUG *****
+                System.out.println("CURRENT COUNTRY: " + currentCountry); // ***** DEBUG *****
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (currentState != null) // location has an "admin area" (state)
+            mMap.addMarker(new MarkerOptions().position(currentLocation).title
+                    ("Marker in " + currentCity + ", " + currentState + ", " + currentCountry));
+        else // location does not have an "admin area" (state)
+            mMap.addMarker(new MarkerOptions().position(currentLocation).title
+                    ("Marker in " + currentCity + ", " + currentCountry));
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 
     @Override
