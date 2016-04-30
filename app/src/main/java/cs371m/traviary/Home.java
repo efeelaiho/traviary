cf9cbe52fde06503b517f0e6949684c570aed38f
Jong Hoon Lim <jhlim84@hotmail.com>
2016-04-30 18:13:00 -0500

    Score and stats work correctly on the Home page.

--- app/src/main/java/cs371m/traviary/Home.java
+++ app/src/main/java/cs371m/traviary/Home.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.widget.Button;
+import android.widget.TextView;
 
 import cs371m.traviary.datastructures.Points;
 
@@ -20,6 +21,11 @@ public class Home extends Fragment {
 
     private Button mLocationButton;
     private Button mAttractionButton;
+
+    private TextView score;
+    private TextView statesStat;
+    private TextView countriesStats;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +37,9 @@ public class Home extends Fragment {
         final MainActivity mainActivity = (MainActivity)getActivity();
         mLocationButton = (Button)v.findViewById(R.id.location_button);
         mAttractionButton = (Button)v.findViewById(R.id.attraction_button);
+        score = (TextView) v.findViewById(R.id.score);
+        statesStat = (TextView) v.findViewById(R.id.states_stats);
+        countriesStats = (TextView) v.findViewById(R.id.country_stats);
 
         mLocationButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -47,7 +56,15 @@ public class Home extends Fragment {
         });
 
         // update points here
-        updatePoints();
+        updateStats();
+
+        score.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         return v;
     }
 
@@ -55,14 +72,16 @@ public class Home extends Fragment {
     public void onResume() {
         super.onResume();
         // make sure to recalculate points onResume
-        updatePoints();
+        updateStats();
     }
 
-    public void updatePoints() {
+    public void updateStats() {
         Points points = new Points(getContext());
         System.out.println("STATES POINTS: " + points.getStatesPoints());
         System.out.println("COUNTRIES POINTS: " + points.getCountriesPoints());
         System.out.println("CHALLENGES POINTS: " + points.getChallengesPoints());
-        System.out.println("TOTAL POINTS: " + points.getTotalPoints());
+        score.setText(Integer.toString(points.getTotalPoints()));
+        statesStat.setText("States Visited: " + points.getStates().size() + " out of 50");
+        countriesStats.setText("Countries Visited: " + points.getCountries().size() + " out of 280");
     }
 }
