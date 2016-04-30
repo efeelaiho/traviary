cf2781be894a671305f8dced3124acb5166a56b5
Jong Hoon Lim <jhlim84@hotmail.com>
2016-04-30 12:42:21 -0500

    Points data structure proposal.

---
+++ app/src/main/java/cs371m/traviary/datastructures/Points.java
@@ -0,0 +1,24 @@
+package cs371m.traviary.datastructures;
+
+/**
+ * Created by jhl2298 on 4/30/2016.
+ */
+public class Points {
+
+    /*
+     * Get states points
+     * Each state is worth 100 points
+     */
+
+    /*
+     * Get countries points
+     * Each country is worth 1000 points
+     */
+
+    /*
+     * Get challenges points
+     * Each challenges are worth:
+     * 
+     */
+
+}
