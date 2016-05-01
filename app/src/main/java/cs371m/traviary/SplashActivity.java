5092004f8315717c84be4de5c7d29152761a3e3f
efeelaiho <efeelaiho15@utexas.edu>
2016-04-30 19:34:26 -0500

    created splash screen

---
+++ app/src/main/java/cs371m/traviary/SplashActivity.java
@@ -0,0 +1,20 @@
+package cs371m.traviary;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by efosaelaiho on 4/30/16.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
