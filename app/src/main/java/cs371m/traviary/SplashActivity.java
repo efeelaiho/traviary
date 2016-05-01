b887a6a1f100db1d90c1e6394f846245cd18d451
efeelaiho <efeelaiho15@utexas.edu>
2016-04-30 19:50:10 -0500

    timed splash screen

--- app/src/main/java/cs371m/traviary/SplashActivity.java
+++ app/src/main/java/cs371m/traviary/SplashActivity.java
@@ -2,6 +2,7 @@ package cs371m.traviary;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -9,12 +10,31 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class SplashActivity extends AppCompatActivity {
 
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
+
