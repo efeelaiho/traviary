5bd49c3ce1616638f622849edef5ff38bc8f6af7
Jong Hoon Lim <jhlim84@hotmail.com>
2016-05-01 17:22:35 -0500

    BitMap conversion code.

--- app/src/main/java/cs371m/traviary/database/SQLiteHelper.java
+++ app/src/main/java/cs371m/traviary/database/SQLiteHelper.java
@@ -5,7 +5,10 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
+import java.io.ByteArrayOutputStream;
 import java.util.HashSet;
 
 /**
@@ -102,6 +105,18 @@ public class SQLiteHelper extends SQLiteOpenHelper {
             return rowInserted;
         }
         return -1;
+    }
+
+    // convert from bitmap to byte array
+    public static byte[] getBytes(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream);
+        return stream.toByteArray();
+    }
+
+    // convert from byte array to bitmap
+    public static Bitmap getImage(byte[] image) {
+        return BitmapFactory.decodeByteArray(image, 0, image.length);
     }
 
     public boolean checkState(String currentState) {
