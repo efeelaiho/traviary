743482f229dbe730927c10a838be46ad10865fba
Jong Hoon Lim <jhlim84@hotmail.com>
2016-05-01 14:13:30 -0500

    Add a tool bar and back button to details activity.

--- app/src/main/java/cs371m/traviary/DetailsActivity.java
+++ app/src/main/java/cs371m/traviary/DetailsActivity.java
@@ -3,6 +3,8 @@ package cs371m.traviary;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,6 +12,8 @@ import android.widget.TextView;
  * Created by jhl2298 on 5/1/2016.
  */
 public class DetailsActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,21 @@ public class DetailsActivity extends ActionBarActivity {
 
         ImageView imageView = (ImageView) findViewById(R.id.image);
         imageView.setImageBitmap(bitmap);
+
+        /* Display the state name on the toolbar */
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
