a0c535926de46744b5d93d4a95e2a1b18a09e341
Jong Hoon Lim <jhlim84@hotmail.com>
2016-05-02 09:08:05 -0500

    States now sortable.

--- app/src/main/java/cs371m/traviary/datastructures/State.java
+++ app/src/main/java/cs371m/traviary/datastructures/State.java
@@ -1,9 +1,11 @@
 package cs371m.traviary.datastructures;
 
+import java.util.Comparator;
+
 /**
  * Created by Jong Hoon Lim on 3/29/2016.
  */
-public class State {
+public class State implements Comparator<State>, Comparable<State> {
 
     public String name;
     public boolean visited;
@@ -13,7 +15,21 @@ public class State {
         this.name = name;
         this.visited = visited;
         this.photoId = photoId;
-
     }
 
+    @Override
+    public int compareTo(State another) {
+        boolean s1Completed = this.visited;
+        boolean s2Completed = another.visited;
+        if (s1Completed && ! s2Completed)
+            return -1;
+        if (s2Completed && ! s1Completed)
+            return +1;
+        return 0;
+    }
+
+    @Override
+    public int compare(State s1, State s2) {
+        return s1.compareTo(s2);
+    }
 }
