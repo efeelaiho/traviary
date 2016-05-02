b3a185e49940cc4b228ff936e8ccf95de7036a83
Jong Hoon Lim <jhlim84@hotmail.com>
2016-05-02 08:55:39 -0500

    Comparator for Challenges.

--- app/src/main/java/cs371m/traviary/datastructures/Challenge.java
+++ app/src/main/java/cs371m/traviary/datastructures/Challenge.java
@@ -1,9 +1,11 @@
 package cs371m.traviary.datastructures;
 
+import java.util.Comparator;
+
 /**
  * Created by jhl2298 on 4/1/2016.
  */
-public class Challenge {
+public class Challenge implements Comparator<Challenge> {
 
     public int challengeNumber;
     public String name;
@@ -21,4 +23,14 @@ public class Challenge {
 
     }
 
+    @Override
+    public int compare(Challenge c1, Challenge c2) {
+        boolean c1Completed = c1.completed;
+        boolean c2Completed = c2.completed;
+        if (c1Completed && ! c2Completed)
+            return +1;
+        if (c2Completed && ! c1Completed)
+            return -1;
+        return 0;
+    }
 }
